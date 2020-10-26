99f307c7a2933378b62eaf13c73224d0acf0ebd8
metalheadpnz <metalheadpnz@gmail.com>
2020-10-26 13:16:50 +0300

    pokemonCard

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./components/login/login";
 import Conform from "./components/conform/conform";
 import Categories from "./components/categories/categories";
 import CardMini from "./components/pokemonCardMini/pokemonCardMini";
+import PokemonCard from "./components/pokemonCard/pokemonCard";
 
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
         <div className="App">
             {/*<Login/>*/}
             {/*<Conform/>*/}
-            <Categories/>
+            {/*<Categories/>*/}
+            <PokemonCard />
 
             {/*<CardMini/>*/}
 
---
+++ src/components/pokemonCard/pokemonCard.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./pokemonCard.module.css";
+
+function PokemonCard() {
+    return (
+        <div className={styles.wrap}>
+            <header className={styles.header}>
+                <div className={styles.back}>{"<"} Back</div>
+                <div className={styles.logout}>Logout</div>
+            </header>
+            <div className={styles.contentContainer}>
+                <div className={styles.leftPanel}>
+                    <div className={styles.bigPic}></div>
+                    <div className={styles.description}>
+                        Does 30 damage plus 20 more damage for each Energy attached to Dark Blastoise but not used to
+                        pay for this attack. You can't add more than 40 damage in this way.
+                    </div>
+                </div>
+                <div className={styles.infoPanel}>
+                    Pokemon name
+                    <br/>
+                    Type
+                    <br/>
+                    Subtype
+                    <hr className={styles.hr}/>
+                    attackDamage
+                    <br/>
+                    attackCost
+                    <br/>
+                    resistances
+                    <br/>
+                    evolvesFrom
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PokemonCard;
