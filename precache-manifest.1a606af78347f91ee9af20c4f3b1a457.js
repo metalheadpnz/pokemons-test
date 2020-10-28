278c7cb725cbc9431d7626fa5e3f21f482eea65d
metalheadpnz <metalheadpnz@gmail.com>
2020-10-28 03:10:04 +0300

    Updates

---
+++ precache-manifest.1a606af78347f91ee9af20c4f3b1a457.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c0c8f59c2443d1a219a34c0dba57e8",
+    "url": "/pokemons-test/index.html"
+  },
+  {
+    "revision": "dc519393a08cf226b1be",
+    "url": "/pokemons-test/static/css/main.5aa410eb.chunk.css"
+  },
+  {
+    "revision": "aed147f3824091714bb5",
+    "url": "/pokemons-test/static/js/2.c8d3a454.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/pokemons-test/static/js/2.c8d3a454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc519393a08cf226b1be",
+    "url": "/pokemons-test/static/js/main.93412481.chunk.js"
+  },
+  {
+    "revision": "53b76d132188767ab269",
+    "url": "/pokemons-test/static/js/runtime-main.e72a5e54.js"
+  },
+  {
+    "revision": "61ead016963fd59c63e84af5356aff26",
+    "url": "/pokemons-test/static/media/Arrow_mini.61ead016.svg"
+  },
+  {
+    "revision": "fc9aaac3615cfcf6127f55fa1a9b5029",
+    "url": "/pokemons-test/static/media/arrowBtn.fc9aaac3.svg"
+  }
+]);
